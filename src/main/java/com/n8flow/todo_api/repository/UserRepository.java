08039c4ff06d5bba572b675f1e2ce0d6a9329036
Pedro Nascimento <7572825+PedroNascimento@users.noreply.github.com>
2026-06-22 19:36:26 -0300

    feat: adiciona repositories de User

---
+++ src/main/java/com/n8flow/todo_api/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.n8flow.todo_api.repository;
+
+import com.n8flow.todo_api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
