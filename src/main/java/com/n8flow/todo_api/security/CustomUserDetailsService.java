2d9229e894ac2daedb5d6f16759fb46cf1b48b4b
Pedro Nascimento <7572825+PedroNascimento@users.noreply.github.com>
2026-06-23 19:16:48 -0300

    feat: implementa arquitetura de segurança stateless com Spring Security e JWT (#4)
    * feat: implementa fluxo de segurança stateless com Spring Security e JWT
    
    * fix: corrige erro de processamento do Lombok

---
+++ src/main/java/com/n8flow/todo_api/security/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.n8flow.todo_api.security;
+
+import com.n8flow.todo_api.entity.User;
+import com.n8flow.todo_api.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + email));
+
+        return new AuthenticatedUser(user);
+    }
+}
